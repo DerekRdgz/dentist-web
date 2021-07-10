b22b7ea236535409b82c3f878f5110aebe197973
andonirdgz <andoni_rdgz@outlook.com>
2021-07-10 18:41:37 -0500

    Removed the "rakurai" view function

--- core/views.py
+++ core/views.py
@@ -3,7 +3,4 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html',{})
 
-def rakurai(request):
-    return render(request, 'rakurai.html',{})
-
 # Create your views here.
